bf618dd7a510aaa6decf8363aaf5b3906adf7a47
Jonas Knobloch <jonas.knobloch@t-online.de>
2023-12-02 20:50:30 +0100

    Add usage example

---
+++ examples/tokenize.rs
@@ -0,0 +1,19 @@
+use morphy::pre_tokenizers::isolate_lemmas::IsolateLemmas;
+use tokenizers::{OffsetReferential, OffsetType, PreTokenizedString, PreTokenizer};
+
+pub fn main() {
+    let pre_tokenizer = IsolateLemmas{
+        add_prefix_space: true,
+        trim_offsets: true,
+        use_regex: true
+    };
+
+    let mut pre_tokenized = PreTokenizedString::from("Hello! How are you? I'm fine, thank you.");
+
+    let _ = pre_tokenizer.pre_tokenize(&mut pre_tokenized);
+
+    println!(
+        "{:?}",
+        pre_tokenized.get_splits(OffsetReferential::Original, OffsetType::Byte)
+    );
+}
