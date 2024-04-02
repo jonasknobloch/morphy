79605f41d488da5e12ad45b5827393995651562b
Jonas Knobloch <jonas.knobloch@t-online.de>
2024-04-02 17:28:44 +0200

    Add sequence to pre-tokenizer wrapper

--- src/pre_tokenizers/mod.rs
+++ src/pre_tokenizers/mod.rs
@@ -9,6 +9,7 @@ use tokenizers::pre_tokenizers::PreTokenizerWrapper as TokenizersPreTokenizerWra
 use tokenizers::{PreTokenizedString, PreTokenizer};
 
 use crate::pre_tokenizers::external::External;
+use crate::pre_tokenizers::sequence::Sequence;
 use crate::pre_tokenizers::tree_split::TreeSplit;
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
@@ -16,6 +17,7 @@ use crate::pre_tokenizers::tree_split::TreeSplit;
 pub enum PreTokenizerWrapper {
     External(External),
     TreeSplit(TreeSplit),
+    Sequence(Sequence),
     TokenizersPreTokenizerWrapper(TokenizersPreTokenizerWrapper),
 }
 
@@ -24,6 +26,7 @@ impl PreTokenizer for PreTokenizerWrapper {
         match self {
             Self::External(ext) => ext.pre_tokenize(normalized),
             Self::TreeSplit(trs) => trs.pre_tokenize(normalized),
+            Self::Sequence(seq) => seq.pre_tokenize(normalized),
             Self::TokenizersPreTokenizerWrapper(ptw) => ptw.pre_tokenize(normalized),
         }
     }
@@ -42,6 +45,20 @@ impl From<External> for PreTokenizerWrapper {
 impl From<TreeSplit> for PreTokenizerWrapper {
     fn from(from: TreeSplit) -> Self {
         PreTokenizerWrapper::TreeSplit(from)
+    }
+}
+
+// PreTokenizerWrapper::from(Sequence::default());
+// PreTokenizerWrapper::TreeSplit(Sequence::default());
+impl From<Sequence> for PreTokenizerWrapper {
+    fn from(from: Sequence) -> Self {
+        PreTokenizerWrapper::Sequence(from)
+    }
+}
+
+impl From<TokenizersPreTokenizerWrapper> for PreTokenizerWrapper {
+    fn from(from: TokenizersPreTokenizerWrapper) -> Self {
+        PreTokenizerWrapper::TokenizersPreTokenizerWrapper(from)
     }
 }
 
